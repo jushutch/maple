f615a5ae40b1a0ec3cafb0bd9052f41caaa68240
Justin Hutchins <justin.hutchins@crowdstrike.com>
2023-06-30 15:53:49 -0500

    Finish Chapter 3.5

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/jushutch/maple/evaluator"
 	"github.com/jushutch/maple/lexer"
 	"github.com/jushutch/maple/parser"
 )
@@ -31,8 +32,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
